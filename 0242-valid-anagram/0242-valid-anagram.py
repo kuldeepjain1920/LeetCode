c0abc474de1bbefa7476ddedfd42f4fe984d4d5d
kuldeepjain1920 <60039892+kuldeepjain1920@users.noreply.github.com>
2026-04-17 15:48:19 -0700

    Time: 7 ms (90.78%), Space: 19.3 MB (95.56%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.py
+++ 0242-valid-anagram/0242-valid-anagram.py
@@ -1,5 +1,6 @@
 class Solution:
     def isAnagram(self, s: str, t: str) -> bool:
+        """
         if len(s) != len(t):
             return False
 
@@ -9,4 +10,7 @@ class Solution:
             tMap[t[i]] = tMap.get(t[i], 0) + 1
         
         return sMap == tMap
+        """
+
+        return Counter(s) == Counter(t)
         

ec4fc6da15b96c2feddaf84b44b6ea84217fd17f
kuldeepjain1920 <60039892+kuldeepjain1920@users.noreply.github.com>
2026-04-17 16:15:27 -0700

    Time: 18 ms (26.99%), Space: 35.7 MB (19.21%) - LeetHub

--- 0217-contains-duplicate/0217-contains-duplicate.py
+++ 0217-contains-duplicate/0217-contains-duplicate.py
@@ -3,8 +3,14 @@ class Solution:
         ##return len(set(nums)) != len(nums)
         nMap = {}
         for i in range(len(nums)):
+            cnt = nMap.get(nums[i], 0)
+            if cnt > 0:
+                return True;
+            nMap[nums[i]] = cnt + 1
+            """
             nMap[nums[i]] = nMap.get(nums[i], 0) + 1
             if nMap.get(nums[i]) > 1:
                 return True
+            """
         return False
         

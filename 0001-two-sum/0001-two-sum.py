f13d2576503e7f3939de9dda9f73656652d56940
Kuldeep Jain <60039892+kuldeepjain1920@users.noreply.github.com>
2026-06-25 16:40:00 -0700

    Improve twoSum implementation
    Refactored twoSum method for better performance and clarity.

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,34 +1,34 @@
 class Solution:
-    """
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        for i in range(len(nums)):
-            for j in range(1,len(nums)):
-                if nums[i] + nums[j] == target:
-                    return [i,j]
-        return None
     """
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
-        parsedNum = {} ## value and index
-        for index, num in enumerate(nums):
-            diff = target - num
-            if diff in parsedNum:
-                return [parsedNum[diff], index]
-            parsedNum[num] = index
+    Runtime beats 5.00% - bad
+    Memory beats 82.5% - good
+    for i in range(len(nums)):
+        for j in range(1,len(nums)):
+            if i != j and nums[i]+nums[j] == target:
+                return [i,j]
+    return None
     """
-    def twoSum(self, nums: List[int], target: int) -> List[int]:
-        index = 0
-        for num in nums:
-            diff = target-num
-            try:
-                otherIndex = nums.index(diff,index+1)
-            except ValueError:
-                otherIndex = -1
-            if otherIndex >= 0:
-                return [index, otherIndex]
-            index += 1
-    """
-
-
-
-
         
+    ### Runtime beats 40.6%
+    ### Memory beats 18.18%
+    map = {} ## value and index
+    for index, num in enumerate(nums):
+        diff = target - num
+        if diff in map:
+            return [map[diff], index]
+        map[num] = index
+    return None
+    """
+    index = 0
+    for num in nums:
+        diff = target-num
+        try:
+            otherIndex = nums.index(diff,index+1)
+        except ValueError:
+            otherIndex = -1
+        if otherIndex >= 0:
+            return [index, otherIndex]
+        index += 1
+    return None
+    """

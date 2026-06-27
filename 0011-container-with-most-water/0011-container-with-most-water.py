11855265604fdb8747b7f353cb9b2a3e0df72bed
Kuldeep Jain <60039892+kuldeepjain1920@users.noreply.github.com>
2026-06-27 16:34:31 -0700

    Time: 47 ms (92.11%), Space: 29.7 MB (37.18%) - LeetHub

---
+++ 0011-container-with-most-water/0011-container-with-most-water.py
@@ -0,0 +1,33 @@
+class Solution:
+    def maxArea(self, height: List[int]) -> int:
+        left = 0
+        right = len(height) - 1
+        area = 0
+        while left <= right:
+            area = max(area, min(height[left], height[right]) * (right - left))
+            if height[left] >= height[right]:
+                right -= 1
+            else:
+                left += 1
+
+        return area
+
+if __name__ == "__main__":
+    sol = Solution()
+
+    test_cases = [
+        ([1,8,6,2,5,4,8,3,7],49),
+        ([1,1],1),
+        ([5,5,5,5,5,5,5,5,5], 40),
+        ([1,2,3,4,5],6),
+        ([6,5,4,3,2,1],9),
+        ([1,1,1,10,1,1,1],6),
+        ([10,1,1,1,1,1,10],60)
+    ]
+    
+    for nums, expected in test_cases:
+        output = sol.maxArea(nums)
+        status = "✅ PASS" if output == expected else "❌ FAIL"
+        print(f"{status} | nums={nums} => {output}")
+
+        

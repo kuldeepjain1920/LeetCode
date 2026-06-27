5b82aeb4910875faccafdc96d17bdf176d3eb62e
Kuldeep Jain <60039892+kuldeepjain1920@users.noreply.github.com>
2026-06-26 18:21:08 -0700

    Time: 45 ms (75.99%), Space: 36.6 MB (37.46%) - LeetHub

--- 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.py
+++ 0128-longest-consecutive-sequence/0128-longest-consecutive-sequence.py
@@ -29,5 +29,21 @@ class Solution:
         return best
         """
 
+if __name__ == "__main__":
+    sol = Solution()
+    test_cases = [
+        ([100,4,200,1,3,2],4),
+        ([0,3,7,2,5,8,4,6,0,1],9),
+        ([1,0,1,2],3),
+        ([5,5,5,5],1),
+        ([1,2,2,3,3,3,4,4,4,4],4),
+        ([-5,-4,5,-3,-2,8,-1,9,0,1,5,8,9],7),
+        ([10],1),
+        ([9,8,7,12,14,15,16,17,19,21,-20,20,-22,18],8)
+    ]
 
+    for nums,expected in test_cases:
+        output = sol.longestConsecutive(nums)
+        status = "✅ PASS " if output == expected else "❌ FAIL"
+        print(f"{status} | nums={nums} => {output}")
         

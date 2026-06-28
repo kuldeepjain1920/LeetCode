73253be406c4a7f60aae212ebaf5ba60e1d8bb9b
Kuldeep Jain <60039892+kuldeepjain1920@users.noreply.github.com>
2026-06-28 07:50:09 -0700

    Time: 13 ms (30.15%), Space: 21.2 MB (24.06%) - LeetHub

--- 0042-trapping-rain-water/0042-trapping-rain-water.py
+++ 0042-trapping-rain-water/0042-trapping-rain-water.py
@@ -1,5 +1,8 @@
 class Solution:
     def trap(self, height: List[int]) -> int:
+        """
+        ## Two pointers Time:O(N), Space O(1)
+        ## Runtime 7 ms Beats 74.4%
         water = 0
         left, right = 0, len(height) - 1
         left_max = right_max = 0
@@ -21,6 +24,26 @@ class Solution:
                 right -= 1
 
         return water
+        """
+        ## Approach 2
+        ## PREFIX-MAX ARRAYS Time:O(N) Space O(N)
+        ## 
+        n = len(height)
+        if n == 0: return 0
+        left_max = [0] * n ## left_max[i] = max_height from 0..i
+        right_max = [0] * n ## right_max[i] = max_height from i..n-1
+
+        left_max[0] = height[0]
+        for i in range(1,n):
+            left_max[i] = max(left_max[i-1], height[i])
+
+        right_max[n-1] = height[n-1]
+        for i in range(n-2, -1, -1):
+            right_max[i] = max(right_max[i+1], height[i])
+
+        return sum(
+            min(left_max[i], right_max[i]) - height[i] for i in range(n)
+        )
 
 if __name__ == "__main__":
     sol = Solution()

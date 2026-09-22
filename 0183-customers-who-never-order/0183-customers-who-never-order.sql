a46a6874aa7d30fdeb632af2c899578836d7c944
Kuldeep Jain <60039892+kuldeepjain1920@users.noreply.github.com>
2026-09-21 20:19:08 -0700

    Time: 591 ms (71.41%), Space: 0B (100%) - LeetHub

--- 0183-customers-who-never-order/0183-customers-who-never-order.sql
+++ 0183-customers-who-never-order/0183-customers-who-never-order.sql
@@ -7,15 +7,17 @@ RIGHT JOIN Orders o
 ON c.id = o.customerId);
 */
 
-/*
+
 SELECT c.name AS Customers
 FROM Customers c
 LEFT JOIN Orders o
 ON c.id = o.customerId
 WHERE o.customerId IS NULL;
-*/
 
+
+/*
 SELECT c.name AS Customers
 FROM Customers c
 WHERE c.id NOT IN
 (select customerId from Orders);
+*/
